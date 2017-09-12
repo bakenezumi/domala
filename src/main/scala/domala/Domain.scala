475e93346cd34e0971832a97a1f2c066d006ff30
hosonishi <hosonishi@gmail.com>
2017-09-12 19:04:52 +0900

    クラス構成見直し

--- src/main/scala/domala/Domain.scala
+++ src/main/scala/domala/Domain.scala
@@ -1,7 +1,9 @@
 package domala
 
-import domala.internal.macros.DomainTypeGenerator
+import domala.internal.macros.{DomaType, TypeHelper}
+import org.scalameta.logger
 
+import scala.collection.immutable.Seq
 import scala.meta._
 
 class Domain extends scala.annotation.StaticAnnotation {
@@ -10,5 +12,60 @@ class Domain extends scala.annotation.StaticAnnotation {
       case cls: Defn.Class => DomainTypeGenerator.generate(cls)
       case _ => abort("@Domain most annotate a class")
     }
+  }
+}
+
+/**
+  * @see [[https://github.com/domaframework/doma/blob/master/src/main/java/org/seasar/doma/internal/apt/DomainTypeGenerator.java]]
+  */
+object DomainTypeGenerator {
+  def generate(cls: Defn.Class) = {
+
+    if (cls.ctor.paramss.flatten.length != 1) abort(cls.pos, domala.message.Message.DOMALA6001.getMessage())
+    val valueParam = cls.ctor.paramss.flatten.headOption.getOrElse(abort(cls.pos, domala.message.Message.DOMALA6001.getMessage()))
+    if(valueParam.name.syntax != "value") abort(cls.pos, domala.message.Message.DOMALA6002.getMessage())
+    val (basicTpe, wrapperSupplier) = TypeHelper.convertToDomaType(valueParam.decltpe.get) match {
+      case DomaType.Basic(_, convertedType, wrapperSupplier) => (convertedType, wrapperSupplier)
+      case _ => abort(cls.pos, domala.message.Message.DOMALA4096.getMessage(valueParam.decltpe.get.toString(), cls.name.syntax, valueParam.name.syntax))
+    }
+
+    val methods = makeMethods(cls.name, cls.ctor, basicTpe)
+
+    val obj = q"""
+    object ${Term.Name(cls.name.syntax)} extends
+      org.seasar.doma.jdbc.domain.AbstractDomainType[
+        $basicTpe, ${cls.name}](
+        $wrapperSupplier: java.util.function.Supplier[org.seasar.doma.wrapper.Wrapper[$basicTpe]]) {
+      def getSingletonInternal() = this
+      val wrapper: java.util.function.Supplier[org.seasar.doma.wrapper.Wrapper[$basicTpe]] = $wrapperSupplier
+      ..$methods
+    }
+    """
+
+    logger.debug(obj)
+    Term.Block(Seq(
+      cls,
+      obj
+    ))
+  }
+
+  protected def makeMethods(clsName: Type.Name, ctor: Ctor.Primary, basicTpe: Type) = {
+    q"""
+    override protected def newDomain(value: $basicTpe): $clsName = {
+      if (value == null) null else ${Term.Name(clsName.toString)}(value)
+    }
+
+    override protected def getBasicValue(domain: $clsName): $basicTpe = {
+      if (domain == null) null else domain.value
+    }
+
+    override def getBasicClass() = {
+      classOf[$basicTpe]
+    }
+
+    override def getDomainClass() = {
+      classOf[$clsName]
+    }
+    """.stats
   }
 }
--- src/main/scala/domala/Embeddable.scala
+++ src/main/scala/domala/Embeddable.scala
@@ -1,7 +1,9 @@
 package domala
 
-import domala.internal.macros.EmbeddableTypeGenerator
+import domala.internal.macros.TypeHelper
+import org.scalameta.logger
 
+import scala.collection.immutable.Seq
 import scala.meta._
 
 class Embeddable extends scala.annotation.StaticAnnotation {
@@ -10,5 +12,80 @@ class Embeddable extends scala.annotation.StaticAnnotation {
       case cls: Defn.Class => EmbeddableTypeGenerator.generate(cls)
       case _ => abort("@Embeddable most annotate a class")
     }
+  }
+}
+
+/**
+  * @see [[https://github.com/domaframework/doma/blob/master/src/main/java/org/seasar/doma/internal/apt/EmbeddableTypeGenerator.java]]
+  */
+object EmbeddableTypeGenerator {
+  def generate(cls: Defn.Class): Term.Block = {
+    val methods = makeMethods(cls.name, cls.ctor)
+
+    val obj = q"""
+    object ${Term.Name(cls.name.syntax)} extends org.seasar.doma.jdbc.entity.EmbeddableType[${cls.name}] {
+      ..$methods
+    }
+    """
+
+    logger.debug(obj)
+    Term.Block(Seq(
+      cls,
+      obj
+    ))
+  }
+
+  protected def makeMethods(clsName: Type.Name, ctor: Ctor.Primary): Seq[Defn.Def] = {
+    Seq(
+      {
+        val params = ctor.paramss.flatten.map { p =>
+          val Term.Param(mods, name, Some(decltpe), default) = p
+          val nameStr = name.syntax
+          val tpe = Type.Name(decltpe.toString)
+          val (basicTpe, newWrapperExpr, domainTpe) = TypeHelper.generateEntityTypeParts(decltpe)
+
+          q"""
+          new domala.jdbc.entity.DefaultPropertyType(
+              entityClass,
+              classOf[$tpe],
+              $basicTpe,
+              $newWrapperExpr,
+              null,
+              null,
+              embeddedPropertyName + "." + $nameStr,
+              "",
+              namingType,
+              true,
+              true,
+              false
+            )
+          """
+        }
+        q"""
+        override def getEmbeddablePropertyTypes[ENTITY](embeddedPropertyName: String, entityClass: Class[ENTITY], namingType: org.seasar.doma.jdbc.entity.NamingType) = {
+          java.util.Arrays.asList(..$params)
+        }
+        """
+      },
+      {
+        val params = ctor.paramss.flatten.map { p =>
+          val Term.Param(mods, name, Some(decltpe), default) = p
+          val nameStr = name.syntax
+          val tpe = Type.Name(decltpe.toString)
+          val (basicTpe, newWrapperExpr, domainTpe) = TypeHelper.generateEntityTypeParts(tpe)
+          q"""
+          { (if(__args.get(embeddedPropertyName + "." + $nameStr) != null )
+              __args.get(embeddedPropertyName + "." + $nameStr)
+            else null).get().asInstanceOf[$tpe] }
+          """
+        }
+
+        q"""
+        override def newEmbeddable[ENTITY](embeddedPropertyName: String,  __args: java.util.Map[String, org.seasar.doma.jdbc.entity.Property[ENTITY, _]]) = {
+          ${Term.Name(clsName.syntax)}(..$params)
+        }
+        """
+      }
+    )
   }
 }
