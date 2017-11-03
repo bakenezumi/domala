3114eea7d13bf8c9c6e08055dd33546d51a709ff
hosonishi <hosonishi@gmail.com>
2017-11-03 18:47:04 +0900

    Add sample

---
+++ sample/src/main/scala/sample/guild/GuildApp.scala
@@ -0,0 +1,111 @@
+package sample.guild
+
+import domala._
+import domala.jdbc.{BatchResult, SelectOptions}
+
+/** Object structure example
+  * {{{
+  * Guild : 1 ------ N   : Character
+  * Guild : 1 ------ 0-1 : GuildHouse
+  * }}}
+  * @see [[http://gakuzzzz.github.io/slides/doma_practice/#1]]
+  * */
+class GuildApp (implicit guildDao: GuildDao, characterDao: CharacterDao, guildHouseDao: GuildHouseDao) {
+
+  def getAllGuildViews: List[GuildView] ={
+    val opt = SelectOptions.get.limit(100).offset(0)
+    val guilds = guildDao.findAll[List[Guild]](opt, _.toList)
+    val ids = guilds.map(_.id)
+    val chars = // 1:N は Map[ID, List] で
+      characterDao.findByGuildIds(ids, _.toList.groupBy(_.guildId))
+    val houses = // 1:0-1 は Map[ID, Entity] で
+      guildHouseDao.findByGuildIds(ids, _.map(house => house.guildId -> house).toMap)
+    guilds.map(buildGuild(chars, houses))
+  }
+
+  private def buildGuild(characters: Map[ID[Guild], List[Character]], houses: Map[ID[Guild], GuildHouse]): Guild => GuildView = {
+    (guild: Guild) =>
+      GuildView(
+        guild,
+        characters.getOrElse(guild.id, Nil),
+        houses.get(guild.id)
+      )
+  }
+}
+case class GuildView(
+  meta: Guild,
+  members: List[Character],
+  house: Option[GuildHouse]
+)
+
+@Holder
+case class ID[E](value: Int)
+
+@Holder
+case class Name(value: String)
+
+@Entity
+case class Guild(
+  id: ID[Guild],
+  name: Name
+)
+
+@Entity
+case class Character(
+  id: ID[Character],
+  name: Name,
+  guildId: ID[Guild]
+)
+
+@Entity
+case class GuildHouse(
+  id: ID[GuildHouse],
+  name: Name,
+  guildId: ID[Guild]
+)
+
+@Dao
+trait GuildDao {
+  @Select("""
+SELECT /*%expand*/* FROM guild WHERE deleted_time IS NULL
+  """, strategy = SelectType.STREAM)
+  def findAll[R](opt: SelectOptions, mapper: Stream[Guild] => R): R
+
+  @BatchInsert
+  def insert(entity: Seq[Guild]): BatchResult[Guild]
+}
+
+@Dao
+trait CharacterDao {
+
+  @Select("""
+SELECT
+  /*%expand*/*
+FROM
+  `character`
+WHERE
+  guild_id IN /*guildIds*/(1, 2)
+  AND deleted_time IS NULL
+  """, strategy = SelectType.STREAM)
+  def findByGuildIds[R](guildIds: Iterable[ID[Guild]], mapper: Stream[Character] => R): R
+
+  @BatchInsert
+  def insert(entity: Seq[Character]): BatchResult[Character]
+}
+
+@Dao
+trait GuildHouseDao {
+  @Select("""
+SELECT
+  /*%expand*/*
+FROM
+  guild_house
+WHERE
+  guild_id IN /*guildIds*/(1, 2)
+  AND deleted_time IS NULL
+  """, strategy = SelectType.STREAM)
+  def findByGuildIds[R](guildIds: Iterable[ID[Guild]], mapper: Stream[GuildHouse] => R): R
+
+  @BatchInsert
+  def insert(entity: Seq[GuildHouse]): BatchResult[GuildHouse]
+}
---
+++ sample/src/test/scala/sample/guild/GuildTestSuite.scala
@@ -0,0 +1,181 @@
+package sample.guild
+
+import domala._
+import domala.jdbc.Config
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import sample.SampleConfig
+
+class GuildTestSuite extends FunSuite with BeforeAndAfter {
+  implicit val config: Config = SampleConfig
+  implicit val guildDao: GuildDao = GuildDao.impl
+  implicit val characterDao: CharacterDao = CharacterDao.impl
+  implicit val guildHouseDao: GuildHouseDao = GuildHouseDao.impl
+  val app = new GuildApp()
+  val envDao: EnvDao = EnvDao.impl
+
+  before {
+    Required {
+      envDao.create()
+    }
+  }
+
+  after {
+    Required {
+      envDao.drop()
+    }
+  }
+
+  test("make guild view") {
+    Required {
+      val guilds = (1 to 10).map(i => Guild(ID(i), Name("g" + i)))
+      val characters = (1 to 50)
+        .zip(
+          Stream
+            .continually(guilds
+              .filter {
+                case Guild(ID(4), _) | Guild(ID(8), _) => false
+                case _                                 => true
+              }
+              .flatMap {
+                case Guild(id @ ID(3), _) => Seq(id, id, id)
+                case Guild(id @ ID(5), _) => Seq(id, id)
+                case Guild(id, _)         => Seq(id)
+              })
+            .flatten
+        )
+        .map {
+          case (id, gid) => Character(ID(id), Name("c" + id), gid)
+        }
+      val houses =
+        (1 to 5).map(i => GuildHouse(ID(i), Name("gh" + i), ID(i * 2)))
+      guildDao.insert(guilds)
+      characterDao.insert(characters)
+      guildHouseDao.insert(houses)
+      val views = app.getAllGuildViews
+      assert(
+        views == List(
+          GuildView(
+            Guild(ID(1), Name("g1")),
+            List(
+              Character(ID(1), Name("c1"), ID(1)),
+              Character(ID(12), Name("c12"), ID(1)),
+              Character(ID(23), Name("c23"), ID(1)),
+              Character(ID(34), Name("c34"), ID(1)),
+              Character(ID(45), Name("c45"), ID(1))
+            ),
+            None
+          ),
+          GuildView(
+            Guild(ID(2), Name("g2")),
+            List(
+              Character(ID(2), Name("c2"), ID(2)),
+              Character(ID(13), Name("c13"), ID(2)),
+              Character(ID(24), Name("c24"), ID(2)),
+              Character(ID(35), Name("c35"), ID(2)),
+              Character(ID(46), Name("c46"), ID(2))
+            ),
+            Some(GuildHouse(ID(1), Name("gh1"), ID(2)))
+          ),
+          GuildView(
+            Guild(ID(3), Name("g3")),
+            List(
+              Character(ID(3), Name("c3"), ID(3)),
+              Character(ID(4), Name("c4"), ID(3)),
+              Character(ID(5), Name("c5"), ID(3)),
+              Character(ID(14), Name("c14"), ID(3)),
+              Character(ID(15), Name("c15"), ID(3)),
+              Character(ID(16), Name("c16"), ID(3)),
+              Character(ID(25), Name("c25"), ID(3)),
+              Character(ID(26), Name("c26"), ID(3)),
+              Character(ID(27), Name("c27"), ID(3)),
+              Character(ID(36), Name("c36"), ID(3)),
+              Character(ID(37), Name("c37"), ID(3)),
+              Character(ID(38), Name("c38"), ID(3)),
+              Character(ID(47), Name("c47"), ID(3)),
+              Character(ID(48), Name("c48"), ID(3)),
+              Character(ID(49), Name("c49"), ID(3))
+            ),
+            None
+          ),
+          GuildView(Guild(ID(4), Name("g4")),
+                    Nil,
+                    Some(GuildHouse(ID(2), Name("gh2"), ID(4)))),
+          GuildView(
+            Guild(ID(5), Name("g5")),
+            List(
+              Character(ID(6), Name("c6"), ID(5)),
+              Character(ID(7), Name("c7"), ID(5)),
+              Character(ID(17), Name("c17"), ID(5)),
+              Character(ID(18), Name("c18"), ID(5)),
+              Character(ID(28), Name("c28"), ID(5)),
+              Character(ID(29), Name("c29"), ID(5)),
+              Character(ID(39), Name("c39"), ID(5)),
+              Character(ID(40), Name("c40"), ID(5)),
+              Character(ID(50), Name("c50"), ID(5))
+            ),
+            None
+          ),
+          GuildView(
+            Guild(ID(6), Name("g6")),
+            List(Character(ID(8), Name("c8"), ID(6)),
+                 Character(ID(19), Name("c19"), ID(6)),
+                 Character(ID(30), Name("c30"), ID(6)),
+                 Character(ID(41), Name("c41"), ID(6))),
+            Some(GuildHouse(ID(3), Name("gh3"), ID(6)))
+          ),
+          GuildView(
+            Guild(ID(7), Name("g7")),
+            List(Character(ID(9), Name("c9"), ID(7)),
+                 Character(ID(20), Name("c20"), ID(7)),
+                 Character(ID(31), Name("c31"), ID(7)),
+                 Character(ID(42), Name("c42"), ID(7))),
+            None
+          ),
+          GuildView(Guild(ID(8), Name("g8")),
+                    Nil,
+                    Some(GuildHouse(ID(4), Name("gh4"), ID(8)))),
+          GuildView(
+            Guild(ID(9), Name("g9")),
+            List(Character(ID(10), Name("c10"), ID(9)),
+                 Character(ID(21), Name("c21"), ID(9)),
+                 Character(ID(32), Name("c32"), ID(9)),
+                 Character(ID(43), Name("c43"), ID(9))),
+            None
+          ),
+          GuildView(
+            Guild(ID(10), Name("g10")),
+            List(Character(ID(11), Name("c11"), ID(10)),
+                 Character(ID(22), Name("c22"), ID(10)),
+                 Character(ID(33), Name("c33"), ID(10)),
+                 Character(ID(44), Name("c44"), ID(10))),
+            Some(GuildHouse(ID(5), Name("gh5"), ID(10)))
+          )
+        ))
+    }
+  }
+  test("nil select") {
+    Required {
+      assert(guildHouseDao.findByGuildIds(Nil, _.toList).isEmpty)
+    }
+  }
+
+  @Dao
+  trait EnvDao {
+    @Script(
+      """
+CREATE TABLE guild(id INT NOT NULL IDENTITY PRIMARY KEY, name VARCHAR(20), deleted_time time);
+CREATE TABLE `character`(id INT NOT NULL IDENTITY PRIMARY KEY, name VARCHAR(20), guild_id INT, deleted_time time,
+  constraint fk_character_guild_id foreign key(guild_id) references guild(id));
+CREATE TABLE guild_house(id INT NOT NULL IDENTITY PRIMARY KEY, name VARCHAR(20), guild_id INT, deleted_time time,
+  constraint fk_guild_house_guild_id foreign key(guild_id) references guild(id));
+""")
+    def create()
+    @Script("""
+DROP TABLE guild_house;
+DROP TABLE `character`;
+DROP TABLE guild;
+""")
+    def drop()
+  }
+
+}
